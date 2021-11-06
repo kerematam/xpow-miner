b4e4c75ead04fa1f3523c65db076a3410409b90a
Kerem ATAM <katam@aefis.com>
2021-11-07 00:57:02 +0300

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # XPOW Miner
 
-Experimental code
+* update .env file with your address and for the min amount of XPOW to mint
+
+* install packages
+```sh
+npm i
+```
+* run for the nounces
+```sh
+npm start
+```
--- src/mine.js
+++ src/mine.js
@@ -1,40 +1,44 @@
-const { keccak256 } = require("web3-utils")
-const Web3 = require('web3');
-const BigNumber = require('big-number');
+require('dotenv').config()
+const { keccak256 } = require('web3-utils')
+const Web3 = require('web3')
+const BigNumber = require('big-number')
+
+const web3 = new Web3()
+
+const ADDRESS = process.env.ADDRESS
+const TARGET_MINT_POWER = process.env.TARGET_MINT_POWER
+const target_mint_power = Number(TARGET_MINT_POWER)
+const n_zeros_str = new Array(target_mint_power + 1).join('0')
 
 const getInterval = () => {
-    const d = + new Date();
-    const interval = (d/3600).toString().slice(0,6);
-    return interval;
-}
+    // make it avaiable once in min
 
-const web3 = new Web3;
-const addr = '0x5F59ee380b05F149407bAEd4b4C2f918cB5Bb399'
+    const d = +new Date()
+    const interval = (d / 3600).toString().slice(0, 6)
+    return interval
+}
 
 const encode = (nonce, addr, interval) => {
     const res = web3.eth.abi.encodeParameters(
-        ['uint256','address', 'uint256'],
+        ['uint256', 'address', 'uint256'],
         [nonce, addr, interval]
-      );
-    return res;
+    )
+    return res
 }
 
-const interval = getInterval();
-
+const interval = getInterval()
 
 // magic start, coz... why not?
-let nonce =  '23834979948776039973182804706599340769739279216212424610326601983427275771595'
-while(true){
-    nonce = BigNumber(nonce).plus(1).toString();
-    const encode_res = encode(nonce, addr, interval)
-    const hash = keccak256(encode_res)
-    const trailing_zeros = hash.slice(2,7)
+let nonce =
+    '23834979948776039973182804706599340769739279216212424610326601983427281798929'
 
-    // 5 zero -> 31
-    // 6 zero  -> 63
-    // 7 zero -> 127
-    // 8 zero -> 255
-    if(trailing_zeros === '00000'){
+while (true) {
+    nonce = BigNumber(nonce).plus(1).toString()
+    const encode_res = encode(nonce, ADDRESS, interval)
+    const hash = keccak256(encode_res)
+    const trailing_zeros = hash.slice(2, target_mint_power + 2)
+
+    if (trailing_zeros === n_zeros_str) {
         console.log('--------')
         console.log('nonce ', nonce)
         console.log('hash', hash)
